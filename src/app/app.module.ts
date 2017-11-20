33168fa634771afab8fd1820b5fb9bf68917872f
Subhod I <subhodpoojary@gmail.com>
2017-11-20 14:33:53 +0530

    Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { DataService } from './services/data.service';
+
 import { AppComponent } from './app.component';
 import { OrdererComponent } from './components/orderer/orderer.component';
 import { PeerComponent } from './components/peer/peer.component';
+import { HelpComponent } from './components/help/help.component';
 
 const appRoutes: Routes = [
   { path: '', component : OrdererComponent },
   { path: 'peer', component : PeerComponent },
+  { path: 'help', component : HelpComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     OrdererComponent,
-    PeerComponent
+    PeerComponent,
+    HelpComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
