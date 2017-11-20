10442c1eb15fd2ba031b3eb500cf03ed8631d435
Subhod I <subhodpoojary@gmail.com>
2017-11-20 19:48:16 +0530

    Peer form without validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { DataService } from './services/data.service';
 
@@ -33,6 +34,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormWizardModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [DataService],
--- src/app/components/peer/peer.component.ts
+++ src/app/components/peer/peer.component.ts
@@ -7,12 +7,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeerComponent implements OnInit {
 
+  state: State;
+  peerForm: Object;
+  peer: Object;
   constructor() { }
 
   ngOnInit() {
+    this.state = State.initialized;
+    this.peer = { name: 'Org1', domain: 'org1.example.com', peerCount: 2, userCount: 1 };
+    this.peerForm = {
+      'PeerOrgs': [
+        {
+          'Name': 'Org1',
+          'Domain': 'org1.example.com',
+          'Template': {
+            'Count': 2
+          },
+          'Users': {
+            'Count': 1
+          }
+        }
+      ]
+    };
+  }
+
+  save(peer) {
+    this.peerForm = {
+      'PeerOrgs': [
+        {
+          'Name': peer.name,
+          'Domain': peer.domain,
+          'Template': {
+            'Count': peer.peerCount
+          },
+          'Users': {
+            'Count': peer.userCount
+          }
+        }
+      ]
+    };
+    this.state = State.save;
   }
 
 }
 
+enum State {
+  initialized = 1,
+  save,
+  cryptoConfigFile,
+  cryptogen,
+  dockerCompose,
+  peerUp
+}
 
 
