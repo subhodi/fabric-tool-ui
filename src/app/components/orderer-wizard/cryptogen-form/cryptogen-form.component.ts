a3eda8bd2687f2511dc109057aceb159f8b6da5e
Subhod I <subhodpoojary@gmail.com>
2017-11-22 16:49:30 +0530

    Orderer pipeline | stable

--- src/app/components/orderer-wizard/cryptogen-form/cryptogen-form.component.ts
+++ src/app/components/orderer-wizard/cryptogen-form/cryptogen-form.component.ts
@@ -6,10 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cryptogen-form.component.css']
 })
 export class CryptogenFormComponent implements OnInit {
-
+  peerForm: Object;
+  peer: Object;
+  requestStatus = RequestStatus;
+  StateStatus: any = {};
+  apiResponse: APIResponse;
   constructor() { }
 
   ngOnInit() {
+    this.peer = { name: 'Org1', domain: 'org1.example.com', peerCount: 2, userCount: 1 };
+    this.peerForm = {
+      'PeerOrgs': [
+        {
+          'Name': 'Org1',
+          'Domain': 'org1.example.com',
+          'Template': {
+            'Count': 2
+          },
+          'Users': {
+            'Count': 1
+          }
+        }
+      ]
+    };
+    this.apiResponse = { 'status': true, 'message': 'Hit submit to start', path: null };
   }
 
+}
+
+enum RequestStatus {
+  pending = 1,
+  success,
+  failure
+}
+
+interface APIResponse {
+  status: boolean;
+  message: string;
+  path: string;
 }
