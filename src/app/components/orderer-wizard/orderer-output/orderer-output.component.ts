f650dcddd6ce5f0f9a9e855a8c665bc5cd9535e3
Subhod I <subhodpoojary@gmail.com>
2017-11-24 17:31:37 +0530

    Title for Charts | Stable commit

--- src/app/components/orderer-wizard/orderer-output/orderer-output.component.ts
+++ src/app/components/orderer-wizard/orderer-output/orderer-output.component.ts
@@ -5,23 +5,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orderer-output.component.css']
 })
 export class OrdererOutputComponent implements OnInit {
-  peers: Peer[] = [];
-  a: any;
   public pieChartLabels: string[] = ['Used', 'Free'];
   public pieChartData: number[] = [70, 30];
-  public pieChartColor: any[] = [{ backgroundColor: ['#009688', '#546E7A'] }]; //00BFA5 546E7A
+  public pieChartColor: any[] = [{ backgroundColor: ['#009688', '#546E7A'] }];
   public pieChartType = 'pie';
   constructor() {
-
-
   }
 
   ngOnInit() {
-    this.peers.push({ containerId: '5d8586f00eae', name: 'ca_peerOrg1', created: '17 hours ago', status: 'Up 17 hours', action: 'remove' });
-    this.peers.push({ containerId: '5d8586f00eae', name: 'ca_peerOrg2', created: '17 hours ago', status: 'Up 17 hours', action: 'remove' });
-    this.peers.push({ containerId: '5d8586f00eae', name: 'ca_peerOrg3', created: '17 hours ago', status: 'Up 17 hours', action: 'remove' });
-    this.peers.push({ containerId: '5d8586f00eae', name: 'ca_peerOrg4', created: '17 hours ago', status: 'Up 17 hours', action: 'remove' });
-    this.peers.push({ containerId: '5d8586f00eae', name: 'ca_peerOrg5', created: '17 hours ago', status: 'Up 17 hours', action: 'remove' });
   }
 
 }
